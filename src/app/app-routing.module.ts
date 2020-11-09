cf15b8137ca7aacf6c52d3fe405a8d2054a2d089
vasylpavliuk <vasylpavliuk007@gmail.com>
2020-11-09 21:35:22 +0200

    [ADDED] collection module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'elements', loadChildren: () => import('./elements/elements.module').then( m => m.ElementsModule) },
+  { path: 'collections', loadChildren: () => import('./collections/collections.module').then( m => m.CollectionsModule) },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
