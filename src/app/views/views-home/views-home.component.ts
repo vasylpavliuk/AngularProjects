f650db5f0bdcbb455371b927ca69be7213ad8057
vasylpavliuk <vasylpavliuk007@gmail.com>
2020-11-09 23:38:23 +0200

    [ADDED] ItemListComponent to ViewsModule

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css']
+})
+export class ViewsHomeComponent implements OnInit {
+  data = [
+    {value: 22, label: '# of Users'},
+    {value: 900, label: 'Revenue'},
+    {value: 50, label: 'Reviews'}
+  ];
+  itemListData = [
+    { image: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80',
+      title: 'Sea shore',
+      description: 'The last night of a two week stay on the North Shore of Oahu, Hawaii.'
+    },
+    { image: 'https://images.unsplash.com/photo-1458668383970-8ddd3927deed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80',
+      title: 'Mountain',
+      description: 'Mountain tops above clouds.'
+    },
+    { image: 'https://images.unsplash.com/photo-1512100356356-de1b84283e18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=409&q=80',
+      title: 'Plane on the water',
+      description: 'Veligandu Maldives.'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
